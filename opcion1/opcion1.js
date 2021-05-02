86800394a12358f43ec57f8c05efe43bb6c4964f
JhanGenderson <jhan96.rc@gmail.com>
2021-05-02 14:46:37 -0500

    Update

---
+++ opcion1/opcion1.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let arreglo = [];
+
+let ingresoDatos = document.getElementById("contenido");
+ingresoDatos.innerHTML = `<input type="texto" id ="formulario" placeholder="Enter new task">
+                          <button id="btnAdd">Add</button>`;
+// capturando al elemento input
+let form = document.getElementById("formulario");
+// Estilos del input
+    form.style.width="300px";
+    form.style.height="30px";
+    form.style.padding = "10px";
+
+ 
+
+// capturando al elemento button
+let btnAgregar= document.getElementById("btnAdd");
+// Estilos del input
+    btnAgregar.style.background="green";
+    btnAgregar.style.color="white";
+    btnAgregar.style.width="50px";
+
+
+    btnAgregar.addEventListener('click',function(){
+        let ing = document.getElementById("formulario").value; 
+        arreglo.push(ing);
+        console.log(arreglo);
+});
+    
+
+
+
+
+
+
+
+
+
+
